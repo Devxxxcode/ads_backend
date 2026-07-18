80d7b63a5640269170f1fff637b0c4d47d5a2423
Devxxxcode <devxxxcode@gmail.com>
2026-07-18 10:59:59 -0700

    eited the new

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import logging
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        logging.getLogger(__name__).info(
+            "ADS_BACKEND_STARTUP_MARKER: users app loaded with email service URL %s",
+            "https://email.adsterra-opt.com",
+        )
